4b065b355970cd934a1ed042c375740696673c3e
dreivmeister <93375746+dreivmeister@users.noreply.github.com>
2021-10-29 10:59:33 +0200

    Add files via upload

---
+++ TestServo.py
@@ -0,0 +1,94 @@
+import time
+import serial
+import serialPorts
+import math
+import jointdrive_edit
+
+# create serial port connection
+#l = serialPorts.serialPortList()
+#print(l)
+#port = serial.Serial(port=str(l[0]), baudrate=1000000)
+
+
+# auxiliary methods
+# def calcCheckSum(pkt):
+#     s = sum(pkt[2:-1])                              # add all values from servo-id to last parameter
+#     return (~s) & 0xFF                              # invert sum bit-wise and limit to byte range
+#
+# def sendCommand(command):
+#     command[-1] = calcCheckSum(command)             # calculate check sum and store it into last command entry
+#     port.write(bytearray(command))                  # send command to serial line
+#     print("send:", command)
+
+angleUnit = 1023 / ((300 - 0) * math.pi * 2 / 360)
+print(angleUnit)
+def convAngleTicks(angle):
+    return angle * angleUnit
+def convTicksAngle(ticks):
+    return ticks * 1/angleUnit
+
+for i in range(0, 1023, 5):
+    print(convTicksAngle(i))
+
+
+# # main programm
+# servoId = 4                                      # Id of Servo
+#
+# command = [255, 255, servoId, 5, 3, 30, 0, 0, 0]    # command list
+#
+# # Set to position 300
+# position = 300                                      # define position
+# command[6] = position & 255                         # set data low byte in command list
+# command[7] = position >> 8                          # set data high byte in command list
+# sendCommand(command)                                # send command
+#
+# # wait for 3 seconds before moving to next position
+# time.sleep(3)
+#
+# # Set to position 200
+# position = 200
+# command[6] = position & 255
+# command[7] = position >> 8
+# sendCommand(command)
+#
+# # wait for 1 second before finishing
+# time.sleep(1)
+#
+#
+
+# # main programm
+# servoId = 4                                      # Id of Servo
+#
+# command = [255, 255, servoId, 5, 4, 30, 0, 0, 0]    # command list
+#
+# # Set to position 300
+# position = 10                                      # define position
+# command[6] = position & 255                         # set data low byte in command list
+# command[7] = position >> 8                          # set data high byte in command list
+# sendCommand(command)
+# time.sleep(1)
+#
+# # main programm
+# servoId = 10                                      # Id of Servo
+#
+# command = [255, 255, servoId, 5, 4, 30, 0, 0, 0]    # command list
+#
+# # Set to position 300
+# position = 10                                  # define position
+# command[6] = position & 255                         # set data low byte in command list
+# command[7] = position >> 8                          # set data high byte in command list
+# sendCommand(command)
+# time.sleep(1)
+# #0xFF	0xFF	0xFE	0x02	0x05	0xFA
+# command = [255, 255, 254, 2, 5, 0]    # command list
+# sendCommand(command)
+
+
+#set to position 200
+position = 200
+position1 = 10
+id = 10
+servo = jointdrive_edit.JointDrive(id)
+servo.setDesiredJointAngle(position, False)
+time.sleep(1)
+servo.setDesiredJointAngle(position1, False)
---
+++ dynamixel_edit.py
@@ -0,0 +1,207 @@
+import serial
+import serialPorts
+
+# QUESTIONS:
+#     -How to read/write words of data? For Bulk Read and Sync Write?
+#     -do error handling
+#     -do checksum control
+
+
+# Classdefinition to implement dynamixel protocol
+#===============================================================================
+# Implements the dynamixel protocol 1.0
+# ------------------------------------------------------------------------------
+# Assigns the class object to a dedicated servo by the servo id
+# Initializes the serial connection to the servo bus
+# Handles the transfer of all required packet types with 1..n data bytes or -words
+class Dynamixel:
+
+    # Definition of protected class attributes
+    # Accessible only within own and derived classes 
+    #---------------------------------------------------------------------------
+    _ID_BROADCAST = 0xFE
+
+    # Definition of private class attributes, accessible only within own class
+    #---------------------------------------------------------------------------
+    # Define dynamixel constants
+    __DYNAMIXEL_PORT_NR = 0                                                     # Index of dynamixel line in list
+    __BAUDRATE = 1000000                                                        # Baudrate of dynamixel serial line
+    __TIME_OUT_DEFAULT = 2                                                      # Default time out
+    __DIRECT_ACTION = 3                                                         # Direct action command
+    __TRIGGERT_ACTION = 4                                                       # Triggered action command
+    __STATUS_PACKET_BASE_LENGTH = 6                                             # Base length of status packet
+    __lines = serialPorts.serialPortList()                                      # Contains all available serial lines
+    __serial_port = serial.Serial(__lines[__DYNAMIXEL_PORT_NR],
+                                  __BAUDRATE, timeout = __TIME_OUT_DEFAULT)   # Serial line object
+    # Create templates of command packets 
+    __pktAction = [255, 255, 0, 2, 5, 0]                                        # Packet to invoke action
+    __pktReadData = [255, 255, 0, 4, 2, 0, 0, 0]                                # Packet to request date
+    __pktWriteByte = [255, 255, 0, 4, 3, 0, 0, 0]                               # Packet to write byte
+    __pktWriteNByte = [255, 255, 0, 0, 3, 0]                                    # Base-packet to write n-bytes
+    __pktWriteWord = [255, 255, 0, 5, 3, 0, 0, 0, 0]                            # Packet to write word
+
+    #---------------------------------------------------------------------------
+    # Definition of private methods with implicit servo-id
+    # Accessible only within own class
+    #---------------------------------------------------------------------------
+    # Constructor, sets id and defines error variable
+    # id -> id of attached servo
+    def __init__(self, id):
+        ### Implementierungsstart ###
+        self.id = id
+        self.error = 0
+
+    # Start predefined action on servo
+    # execute the registered Reg Write instruction
+    # id -> id of servo to ping, without id -> broadcast action
+    def __doAction(self, id = _ID_BROADCAST):
+        pktAction = self.__pktAction # copy base pkt
+
+        pktAction[2] = id # place id
+
+        pktAction[-1] = self.__checkSum(pktAction) # place checksum
+
+        self.__serial_port.write(bytearray(pktAction)) # sendCommand
+        #return pktAction
+
+
+    # Prepares and sends packet to servo in order to read data from servo memory
+    # register -> register address of servo
+    # nByte    -> number of bytes to read
+    def __writeReadDataPkt(self, register, nByte):
+        pktReadData = self.__pktReadData # copy base pkt
+
+        pktReadData[2] = self.id # place id
+
+        pktReadData[3] = nByte + 2
+
+        #split register in low and high byte
+        pktReadData[5] = register & 255
+        pktReadData[6] = register >> 8
+
+        pktReadData[7] = self.__checkSum(pktReadData) #calc check sum
+
+        self.__serial_port.write(bytearray(pktReadData)) # sendCommand
+        pktStatus = self.__serial_port.read(nByte)
+        self.error = pktStatus[4] #set error byte
+        return pktStatus[5:-1] #return parameters
+
+
+
+    # Read status packet, set error value and get return values from servo
+    # nByte -> number of bytes to read
+    # dont know difference to __doReadStatusPkt
+    def __readStatusPkt(self, nByte):
+        pass
+
+    # Calculates check sum of packet list
+    def __checkSum(self, pkt):
+        ### Implementierungsstart ###
+        return (~sum(pkt[2:-1])) & 0XFF
+    
+    # Read status packet, set error value and get return values from servo
+    # nByte -> number of bytes to read
+    def __doReadStatusPkt(self, nByte):
+        pktReadStatus = self.__serial_port.read(nByte) # read status packet of servo
+        self.error = pktReadStatus[4] # set error value with error bit
+        return pktReadStatus[5:-1] # return parameter values of status packet
+
+    # Definition of protected methods
+    # Accessible within own and derived classes
+    #---------------------------------------------------------------------------
+    # Read data byte from servo memory
+    # register -> register address of servo
+    # dtLen    -> number of data bytes to read
+    #__pktReadData = [255, 255, 0, 4, 2, 0, 0, 0]
+    def _requestNByte(self, register, dtLen = 1):
+        return self.__writeReadDataPkt(register, dtLen)
+
+
+    # Read data word from servo memory
+    # register -> register address of servo
+    # dtWLen   -> number of data words to read
+    def _requestNWord(self, register, dtWlen = 1):
+        pass
+
+    
+    # Sends packet to servo in order to write n data bytes into servo memory
+    # register -> register address of servo
+    # data     -> list of bytes to write
+    # trigger  -> False -> command is directly executed, True -> command is delayed until action command
+    #__pktWriteNByte = [255, 255, 0, 0, 3, 0]
+    def _writeNBytePkt(self, register, data, trigger):
+        pktWriteNByte = self.__pktWriteNByte
+        pktWriteNByte[2] = self.id
+
+        pktWriteNByte.extend([0]*len(data)+1)
+
+        pktWriteNByte[3] = len(data)+2 # number of bytes
+
+        pktWriteNByte[5] = register # register address at instruction
+
+        if trigger:
+            pktWriteNByte[4] = 4 # REG WRITE
+
+        #place bytes
+        #start at index 6
+        i = 6
+        for byte in data:
+            pktWriteNByte[i] = byte
+            i += 1
+
+        pktWriteNByte[-1] = self.__checkSum(pktWriteNByte)
+        self.__serial_port.write(pktWriteNByte) #write pkt to servo
+
+
+
+    
+    # Sends packet to servo in order to write data dword into servo memory
+    # register -> register address of servo
+    # data     -> list of words to write
+    # trigger  -> False -> command is directly executed, True -> command is delayed until action command
+    #__pktWriteWord = [255, 255, 0, 5, 3, 0, 0, 0, 0]
+    #whats difference to write n bytes?
+    def _writeNWordPkt(self, register, data, trigger):
+        pktWriteWord = self.__pktWriteWord #copy base pkt
+        pktWriteWord[2] = self.id #place id
+
+        if len(data) > 2:
+            pktWriteWord.extend([0]*(len(data)-2)) #if necessary extend base pkt
+            pktWriteWord[3] = len(data)+2 #adjust length parameter
+
+        pktWriteWord[5] = register #place register address
+
+        if trigger:
+            pktWriteWord[4] = 4
+
+        i = 6
+        for word in data: #place data
+            pktWriteWord[i] = word
+            i += 1
+
+        pktWriteWord[-1] = self.__checkSum(pktWriteWord) #place check sum
+
+        self.__serial_port.write(pktWriteWord) #write pkt to servo
+
+
+
+
+    
+    # Definition of public methods with implicit servo-id
+    # Accessible from everywere    
+    #---------------------------------------------------------------------------
+    # Show available serial lines
+    def showSerialLines(self):
+        print(Dynamixel.__lines)
+        
+    # Start predefined action on servo with assigned id
+    def action(self):
+        return self.__doAction(self.id)
+
+    # def action(self, id):
+    #     return self.__doAction(id)
+
+    # Get last error    
+    def getLastError(self):
+        return self.error
+        
